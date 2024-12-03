63d8d727ad7bee4c45e8c332477357d1c19ced6a
Tahir Atakan Can <atkan4202@gmail.com>
2024-12-03 15:43:14 +0300

    Using GetMapping and RequestMapping

--- spring-rest-api/src/main/java/com/tahiratakancan/controller/RestEmployeeController.java
+++ spring-rest-api/src/main/java/com/tahiratakancan/controller/RestEmployeeController.java
@@ -1,11 +1,21 @@
 package com.tahiratakancan.controller;
 
 
+import com.tahiratakancan.model.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/rest/api")
 public class RestEmployeeController {
 
+    @GetMapping(path = "/employee-list")
+    public List<Employee> getAllEmployeeList(){
 
+        return null;
+    }
 
 }
