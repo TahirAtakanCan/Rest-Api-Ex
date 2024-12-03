9659196ede33f1af9c97f33e8762d50cd5a0a67a
Tahir Atakan Can <atkan4202@gmail.com>
2024-12-03 15:29:36 +0300

    first commit

---
+++ spring-rest-api/src/main/java/com/tahiratakancan/controller/RestEmployeeController.java
@@ -0,0 +1,11 @@
+package com.tahiratakancan.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestEmployeeController {
+
+
+
+}
---
+++ spring-rest-api/src/main/java/com/tahiratakancan/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.tahiratakancan.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class EmployeeRepository {
+
+}
